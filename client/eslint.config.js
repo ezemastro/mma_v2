dee986dc2ee33199f64a6cc35216878c27a8ac3d
ezemastro <mastropietroezequiel@gmail.com>
2026-04-19 22:59:26 +0000

    feat: update ESLint configuration and add TypeScript ESLint dependencies

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,12 +1,13 @@
-import eslintPluginAstro from 'eslint-plugin-astro';
-import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginAstro from "eslint-plugin-astro";
+import eslintConfigPrettier from "eslint-config-prettier";
+import tseslint from "typescript-eslint";
 
 export default [
+  ...tseslint.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
 
   {
-    rules: {
-    }
+    rules: {},
   },
 
   eslintConfigPrettier,
